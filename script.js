1b302c26767f1e3b42b4d83766b384c281a84428
kubais <30256909+kubais@users.noreply.github.com>
2024-02-23 21:14:35 +0100

    Add counter with condition for the game win.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let counter = 0;
 const rock = 'rock';
 const paper = 'paper';
 const scissors = 'scissors';
@@ -21,32 +21,55 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
         roundResult = "Result is a tie!";
+        counter += 0;
         } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
             roundResult = "You lose! Paper beats rock";
+            counter += -1;
         }      
          else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
             roundResult = "You win! Rock beats Scissors";    
+            counter += 1;
          }
 
          else if ((playerSelection == "paper") && (computerSelection == "rock")) {
             roundResult = "You win! Paper beats rock";    
+            counter += 1;
          }
          else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
             roundResult = "You Lose! Scissors beat paper";    
+            counter += -1;
          }
          else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
             roundResult = "You lose! Rock beats Scissors";    
+            counter += -1;
          }
          else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
             roundResult = "You win! Scissors beat paper";    
+            counter += 1;
          }
-
-    return roundResult;
-         
+    console.log(roundResult);
+    console.log(counter);
+    return roundResult, counter; 
+    
+    
 
     }
 
+function playGame() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    if (counter >= 3) {
+        alert("You won the game!");
+        } 
+    else alert("You lost the game, maybe next time");
+}
+
+
+
 //  const playerSelection = "rock";
 //  const computerSelection = getComputerChoice();
- console.log(playRound(playerSelection, computerSelection));
+//  console.log(playRound(playerSelection, computerSelection));
 
