eb8fce0f0362570a66a912d3baa68f2e5626b7f2
kubais <30256909+kubais@users.noreply.github.com>
2024-02-23 17:31:31 +0100

    Create file structure and first function.
    Make function getComputerChoice to return randomly rock, paper or scissors.

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+   
+   let result = ["rock","paper","scissors"][Math.floor(Math.random() * 3)];
+
+   console.log(result);  
+
+}
+
+
+// let array = ["rock", "paper", "scissors"];
+// let index = Math.floor(Math.random() * array.length);
+// let chosenText = array[index];
+// console.log(chosenText);
