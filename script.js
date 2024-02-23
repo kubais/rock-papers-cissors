0103b3ff953695046effe3878c9d6a7eedf06f0a
kubais <30256909+kubais@users.noreply.github.com>
2024-02-23 20:30:28 +0100

    Add the rest of conditions and case insensitivity.
    Add all 9 possibilities into conditions.
    Add case insensitivity by converting prompt toLowerCase.
    Add console logs for computer and player inputs with results of a round.

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
+
+const rock = 'rock';
+const paper = 'paper';
+const scissors = 'scissors';
+
+let playerSelection = '';
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
    
    let result = ["rock","paper","scissors"][Math.floor(Math.random() * 3)];
-
-   console.log(result);  
+    
+   return result;
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Please enter your choice: rock, paper, scissors");
+    playerSelection = prompt("Please enter your choice: rock, paper, scissors").toLowerCase();
     computerSelection = getComputerChoice();
-    
-    if ((playerSelection = "rock") && (computerSelection = "paper")) {
-          alert("You lose! Paper beats rock");
-    }      
-        else if ((playerSelection = "rock") && (computerSelection = "scissors")) {
-            alert("You win! Rock beats Scissors");    
-        }
+    console.log('computer chose: ', computerSelection);
+    console.log('player chose: ', playerSelection);
 
-        else if ((playerSelection = "rock") && (computerSelection = "rock")) {
-            alert("Tie! Rock equals rock");    
-        }
+    if (playerSelection == computerSelection) {
+        alert("Result is a tie!");
+        } else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+            alert("You lose! Paper beats rock");
+        }      
+         else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+             alert("You win! Rock beats Scissors");    
+         }
 
-    
+         else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+             alert("You win! Paper beats rock");    
+         }
+         else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+             alert("You Lose! Scissors beat paper");    
+         }
+         else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+             alert("You lose! Rock beats Scissors");    
+         }
+         else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+             alert("You win! Scissors beat paper");    
+         }
+         
 
     }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+//  const playerSelection = "rock";
+//  const computerSelection = getComputerChoice();
+//  console.log(playRound(playerSelection, computerSelection));
 
